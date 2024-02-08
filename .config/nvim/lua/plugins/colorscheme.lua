c9ea6f940012d93866431a5609f3466b510fc336
johnalexmills <johnalexmills@icloud.com>
2024-02-08 06:49:19 -0500

    removing transparency

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -6,7 +6,7 @@ return {
   config = function()
     require("catppuccin").setup {
       flavour = "mocha",
-      transparent_background = true,
+      -- transparent_background = true,
     }
     vim.cmd.colorscheme "catppuccin-mocha"
   end,
