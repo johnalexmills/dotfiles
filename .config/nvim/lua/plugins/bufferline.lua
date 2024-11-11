7cbafdd4c76237b2391d9a663adc6bf4c7a2e535
johnalexmills <johnalexmills@icloud.com>
2024-11-10 21:24:43 -0500

    adding hardtime

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -2,12 +2,10 @@ return {
   "akinsho/bufferline.nvim",
   dependencies = "nvim-tree/nvim-web-devicons",
   event = { "InsertEnter", "BufReadPre", "BufAdd", "BufNew", "BufReadPost" },
-  config = function()
-    require("bufferline").setup {
-      options = {
-        separator_style = "slope",
-        diagnostics = "nvim_lsp",
-      },
-    }
-  end,
+  opts = {
+    options = {
+      separator_style = "slope",
+      diagnostics = "nvim_lsp",
+    },
+  },
 }
