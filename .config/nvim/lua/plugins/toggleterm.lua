ec240806490870113e144f42b26d8f9ddb806441
johnalexmills <johnalexmills@icloud.com>
2024-11-15 08:28:49 -0500

    removing lazygit command

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -21,11 +21,11 @@ return {
     }
 
     local Terminal = require("toggleterm.terminal").Terminal
-    local lazygit = Terminal:new { cmd = "lazygit", hidden = true }
-
-    function _LAZYGIT_TOGGLE()
-      lazygit:toggle()
-    end
+    -- local lazygit = Terminal:new { cmd = "lazygit", hidden = true }
+    --
+    -- function _LAZYGIT_TOGGLE()
+    --   lazygit:toggle()
+    -- end
 
     local node = Terminal:new { cmd = "node", hidden = true }
 
