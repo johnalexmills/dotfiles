1c5f628ca89922c89d9f611fbc009ebbb8224a54
johnalexmills <johnalexmills@icloud.com>
2024-11-17 18:54:58 -0500

    working on debugging

--- .config/nvim/lua/plugins/debugging.lua
+++ .config/nvim/lua/plugins/debugging.lua
@@ -1,31 +1,26 @@
 return {
-  -- "mfussenegger/nvim-dap",
-  -- dependencies = {
-  --   "mfussenegger/nvim-dap-python",
-  --   "rcarriga/nvim-dap-ui",
-  -- },
-  -- config = function()
-  --   require("dapui").setup()
-  --   require("dap-python").setup "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
-  --
-  --   local dap, dapui = require "dap", require "dapui"
-  --
-  --   dap.listeners.before.attach.dapui_config = function()
-  --     dapui.open()
-  --   end
-  --   dap.listeners.before.launch.dapui_config = function()
-  --     dapui.open()
-  --   end
-  --   dap.listeners.before.event_terminated.dapui_config = function()
-  --     dapui.close()
-  --   end
-  --   dap.listeners.before.event_exited.dapui_config = function()
-  --     dapui.close()
-  --   end
-  --
-  --   vim.keymap.set("n", "<Leader>dt", ":DapToggleBreakpoint<CR>")
-  --   vim.keymap.set("n", "<Leader>dc", ":DapContinue<CR>")
-  --   vim.keymap.set("n", "<Leader>dx", ":DapTerminate<CR>")
-  --   vim.keymap.set("n", "<Leader>do", ":DapStepOver<CR>")
-  -- end,
+  "mfussenegger/nvim-dap",
+  dependencies = {
+    "rcarriga/nvim-dap-ui",
+    "nvim-neotest/nvim-nio",
+    "mfussenegger/nvim-dap-python",
+  },
+  config = function()
+    local dap, dapui = require "dap", require "dapui"
+    require("dap-python").setup "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
+    require("dapui").setup()
+
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
+  end,
 }
