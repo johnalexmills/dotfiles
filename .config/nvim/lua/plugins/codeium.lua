fdfa550bdc02ee6f49cc81f323f0ce2ad8b209bd
johnalexmills <johnalexmills@icloud.com>
2025-02-10 21:54:02 -0500

    updating codeium to nvim vs the vim version

--- .config/nvim/lua/plugins/codeium.lua
+++ .config/nvim/lua/plugins/codeium.lua
@@ -1,12 +1,17 @@
 return {
-    'Exafunction/codeium.vim',
-     cmd = "Codeium",
-     event = { "BufEnter" },
-     config = function()
-    -- Change '<C-g>' here to any keycode you like.
-    vim.keymap.set('i', '<C-g>', function () return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
-    vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
-    vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true, silent = true })
-    vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
-  end
+  "Exafunction/codeium.nvim",
+  cmd = "Codeium",
+  event = { "BufEnter" },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "hrsh7th/nvim-cmp",
+  },
+  config = function()
+    require("codeium").setup {
+            enable_chat = true,
+            virtual_text = {
+                enabled = true
+            }
+        }
+  end,
 }
