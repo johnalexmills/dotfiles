730b34f3258b04700af266633f882dc40fcdef6a
johnalexmills <johnalexmills@icloud.com>
2025-02-11 19:20:22 -0500

    changing codeium accept

--- .config/nvim/lua/plugins/codeium.lua
+++ .config/nvim/lua/plugins/codeium.lua
@@ -6,12 +6,27 @@ return {
     "nvim-lua/plenary.nvim",
     "hrsh7th/nvim-cmp",
   },
+
   config = function()
     require("codeium").setup {
-            enable_chat = true,
-            virtual_text = {
-                enabled = true
-            }
-        }
+      enable_chat = true,
+      virtual_text = {
+        enabled = true,
+        key_bindings = {
+          -- Accept the current completion.
+          accept = "<M-l>",
+          -- Accept the next word.
+          accept_word = false,
+          -- Accept the next line.
+          accept_line = false,
+          -- Clear the virtual text.
+          clear = false,
+          -- Cycle to the next completion.
+          next = "<M-]>",
+          -- Cycle to the previous completion.
+          prev = "<M-[>",
+        },
+      },
+    }
   end,
 }
