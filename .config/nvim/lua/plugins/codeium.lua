645cdd1785cd44f73d7c3b940cb4601e7e4805a9
johnalexmills <johnalexmills@icloud.com>
2025-04-15 18:14:12 -0400

    switching back to codium

--- .config/nvim/lua/plugins/codeium.lua
+++ .config/nvim/lua/plugins/codeium.lua
@@ -1,32 +1,32 @@
 return {
-  -- "Exafunction/codeium.nvim",
-  -- cmd = "Codeium",
-  -- event = { "BufEnter" },
-  -- dependencies = {
-  --   "nvim-lua/plenary.nvim",
-  --   "hrsh7th/nvim-cmp",
-  -- },
-  --
-  -- config = function()
-  --   require("codeium").setup {
-  --     enable_chat = true,
-  --     virtual_text = {
-  --       enabled = true,
-  --       key_bindings = {
-  --         -- Accept the current completion.
-  --         accept = "<M-l>",
-  --         -- Accept the next word.
-  --         accept_word = false,
-  --         -- Accept the next line.
-  --         accept_line = false,
-  --         -- Clear the virtual text.
-  --         clear = false,
-  --         -- Cycle to the next completion.
-  --         next = "<M-]>",
-  --         -- Cycle to the previous completion.
-  --         prev = "<M-[>",
-  --       },
-  --     },
-  --   }
-  -- end,
+  "Exafunction/codeium.nvim",
+  cmd = "Codeium",
+  event = { "BufEnter" },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "hrsh7th/nvim-cmp",
+  },
+
+  config = function()
+    require("codeium").setup {
+      enable_chat = true,
+      virtual_text = {
+        enabled = true,
+        key_bindings = {
+          -- Accept the current completion.
+          accept = "<M-l>",
+          -- Accept the next word.
+          accept_word = false,
+          -- Accept the next line.
+          accept_line = false,
+          -- Clear the virtual text.
+          clear = false,
+          -- Cycle to the next completion.
+          next = "<M-]>",
+          -- Cycle to the previous completion.
+          prev = "<M-[>",
+        },
+      },
+    }
+  end,
 }
--- .config/nvim/lua/plugins/supermaven.lua
+++ .config/nvim/lua/plugins/supermaven.lua
@@ -1,13 +1,13 @@
 return {
-  "supermaven-inc/supermaven-nvim",
-  event = { "InsertEnter", "LspAttach" },
-  config = function()
-    require("supermaven-nvim").setup {
-      keymaps = {
-        accept_suggestion = "<M-l>",
-        clear_suggestion = "<M-]>",
-        accept_word = "<M-w>",
-      },
-    }
-  end,
+  -- "supermaven-inc/supermaven-nvim",
+  -- event = { "InsertEnter", "LspAttach" },
+  -- config = function()
+  --   require("supermaven-nvim").setup {
+  --     keymaps = {
+  --       accept_suggestion = "<M-l>",
+  --       clear_suggestion = "<M-]>",
+  --       accept_word = "<M-w>",
+  --     },
+  --   }
+  -- end,
 }
