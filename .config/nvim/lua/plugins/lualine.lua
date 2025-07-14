1009a8808372dc65cad07c7d6efd7ef6a9b2b443
johnalexmills <johnalexmills@icloud.com>
2025-07-14 16:46:06 -0400

    adding noice...again

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -72,7 +72,29 @@ return {
       sections = {
         lualine_a = { mode },
         lualine_b = { "branch" },
-        lualine_c = { filename, { "copilot", show_colors = true } },
+        lualine_c = {
+          filename,
+          { "copilot", show_colors = true },
+          {
+            require("noice").api.status.message.get_hl,
+            cond = require("noice").api.status.message.has,
+          },
+          {
+            require("noice").api.status.command.get,
+            cond = require("noice").api.status.command.has,
+            color = { fg = "#ff9e64" },
+          },
+          {
+            require("noice").api.status.mode.get,
+            cond = require("noice").api.status.mode.has,
+            color = { fg = "#ff9e64" },
+          },
+          {
+            require("noice").api.status.search.get,
+            cond = require("noice").api.status.search.has,
+            color = { fg = "#ff9e64" },
+          },
+        },
         lualine_x = {
           lsp_info,
           diagnostics,
