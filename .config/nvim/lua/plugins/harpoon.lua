447adb293aafca1ab65cd3f57fc114a30eeb8a54
johnalexmills <johnalexmills@icloud.com>
2025-07-19 10:11:14 -0400

    changing harpoon keymaps to alt

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -10,10 +10,10 @@ return {
     vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end, { desc = "Harpoon: Mark File" })
     vim.keymap.set("n", "<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Toggle Harpoon Menu" })
 
-    vim.keymap.set("n", "<C-n>", function() harpoon:list():select(1) end, { desc = "Harpoon File 1" })
-    vim.keymap.set("n", "<C-e>", function() harpoon:list():select(2) end, { desc = "Harpoon File 2" })
-    vim.keymap.set("n", "<C-i>", function() harpoon:list():select(3) end, { desc = "Harpoon File 3" })
-    vim.keymap.set("n", "<C-o>", function() harpoon:list():select(4) end, { desc = "Harpoon File 4" })
+    vim.keymap.set("n", "<M-n>", function() harpoon:list():select(1) end, { desc = "Harpoon File 1" })
+    vim.keymap.set("n", "<M-e>", function() harpoon:list():select(2) end, { desc = "Harpoon File 2" })
+    vim.keymap.set("n", "<M-i>", function() harpoon:list():select(3) end, { desc = "Harpoon File 3" })
+    vim.keymap.set("n", "<M-o>", function() harpoon:list():select(4) end, { desc = "Harpoon File 4" })
 
   end,
 }
