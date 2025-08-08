5ccc695bc5841d355ea7c64ef94fb986a1050665
johnalexmills <johnalexmills@icloud.com>
2025-08-07 21:10:14 -0400

    adding render-markdown

---
+++ .config/nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,16 @@
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  opts = {
+    file_types = { "markdown", "codecompanion" },
+    code = {
+      sign = false,
+      width = "block",
+      right_pad = 1,
+    },
+    heading = {
+      sign = false,
+      icons = { "◉ ", "○ ", "✸ ", "✿ " },
+    },
+  },
+  ft = { "markdown", "codecompanion" },
+}
