7e11715c1c9ab120e8b6a3e48bc86c9dbf773f0b
johnalexmills <johnalexmills@icloud.com>
2025-08-10 17:23:02 -0400

    adding fidget

--- .config/nvim/lua/plugins/fidget.lua
+++ .config/nvim/lua/plugins/fidget.lua
@@ -1,3 +1,31 @@
 return {
   "j-hui/fidget.nvim",
+  cmd = "Fidget",
+  event = "LspAttach",
+  config = function()
+    require("fidget").setup({
+      progress = {
+        display = {
+          render_limit = 16,
+          done_ttl = 3,
+          done_icon = "✔",
+          done_style = "Constant",
+          progress_icon = { pattern = "dots", period = 1 },
+          progress_style = "WarningMsg",
+          group_style = "Title",
+          overrides = {
+            rust_analyzer = { name = "rust-analyzer" },
+          },
+        },
+      },
+      notification = {
+        window = {
+          winblend = 100,
+          border = "none",
+          align = "bottom",
+          relative = "editor",
+        },
+      },
+    })
+  end,
 }
