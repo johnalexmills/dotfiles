f5a77fe6441dfacbd2f0d41938691ac3ee2bb3be
johnalexmills <johnalexmills@icloud.com>
2025-08-11 07:50:06 -0400

    updating render-markdown

--- .config/nvim/lua/plugins/render-markdown.lua
+++ .config/nvim/lua/plugins/render-markdown.lua
@@ -1,15 +1,18 @@
 return {
   "MeanderingProgrammer/render-markdown.nvim",
-  opts = {
-    file_types = { "markdown", "copilot-chat" },
-    code = {
-      sign = false,
-      width = "block",
-      right_pad = 1,
-    },
-    heading = {
-      sign = false,
-      icons = { "◉ ", "○ ", "✸ ", "✿ " },
-    },
-  },
+  event = "VeryLazy",
+  config = function()
+    require("render-markdown").setup {
+      file_types = { "markdown", "copilot-chat" },
+      code = {
+        sign = false,
+        width = "block",
+        right_pad = 1,
+      },
+      heading = {
+        sign = false,
+        icons = { "◉ ", "○ ", "✸ ", "✿ " },
+      },
+    }
+  end,
 }
