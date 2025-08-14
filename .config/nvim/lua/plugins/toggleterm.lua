3109e38d34150ecca2323f63c6661c5796d55197
johnalexmills <johnalexmills@icloud.com>
2025-08-14 13:23:34 -0400

    adjusting toggleterm defaults

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -7,7 +7,7 @@ return {
         if term.direction == "horizontal" then
           return 15
         elseif term.direction == "vertical" then
-          return vim.o.columns * 0.4
+          return vim.o.columns * 0.5
         end
         return 20
       end,
@@ -16,7 +16,7 @@ return {
       start_in_insert = true,
       insert_mappings = true,
       persist_size = true,
-      direction = "horizontal",
+      direction = "vertical",
       close_on_exit = true,
       shell = vim.o.shell,
       float_opts = {
