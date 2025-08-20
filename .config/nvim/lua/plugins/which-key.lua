15a6cd15f1c0f7e7a4edeb0dceedfac23657d4e7
johnalexmills <johnalexmills@icloud.com>
2025-08-20 13:08:54 -0400

    correcting which-key config
    this config was using opts in the wrong way, replaced with setup.

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -1,30 +1,30 @@
 return {
   "folke/which-key.nvim",
   event = "VeryLazy",
-  opts = {
-    preset = "modern",
-    icons = {
-      breadcrumb = "»",
-      separator = "➜",
-      group = "+",
-    },
-    win = {
-      border = "rounded",
-      padding = { 2, 2, 2, 2 },
-      wo = {
-        winblend = 0,
-      },
-    },
-    layout = {
-      height = { min = 4, max = 25 },
-      width = { min = 20, max = 50 },
-      spacing = 3,
-      align = "left",
-    },
-    show_help = true,
-    show_keys = true,
-  },
   config = function()
+    require("which-key").setup({
+      preset = "modern",
+      icons = {
+        breadcrumb = "»",
+        separator = "➜",
+        group = "+",
+      },
+      win = {
+        border = "rounded",
+        padding = { 2, 2, 2, 2 },
+        wo = {
+          winblend = 0,
+        },
+      },
+      layout = {
+        height = { min = 4, max = 25 },
+        width = { min = 20, max = 50 },
+        spacing = 3,
+        align = "left",
+      },
+      show_help = true,
+      show_keys = true,
+    })
     require("which-key").add(require("keymaps"))
   end,
 }
