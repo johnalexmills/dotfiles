92f75843b9eea3eb403d6d75e06fd925fd93366f
johnalexmills <johnalexmills@icloud.com>
2025-08-20 13:05:36 -0400

    correcting colorscheme config
    config was not using setup call which doesn't work with lazy for calling
    the colorscheme.

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -2,48 +2,48 @@ return {
   "catppuccin/nvim",
   lazy = false,
   priority = 1000,
-  name = "catppuccin-mocha",
-  opts = {
-    flavour = "mocha",
-    transparent_background = true,
-    term_colors = true,
-    compile_path = vim.fn.stdpath("cache") .. "/catppuccin",
-    styles = {
-      comments = { "italic" },
-      conditionals = { "italic" },
-      keywords = { "italic" },
-      types = { "italic" },
-    },
-    integrations = {
-      cmp = true,
-      gitsigns = true,
-      treesitter = true,
-      which_key = true,
-      telescope = { enabled = true },
-      illuminate = { enabled = true, lsp = false },
-      native_lsp = {
-        enabled = true,
-        virtual_text = {
-          errors = { "italic" },
-          hints = { "italic" },
-          warnings = { "italic" },
-          information = { "italic" },
-          ok = { "italic" },
-        },
-        underlines = {
-          errors = { "underline" },
-          hints = { "underline" },
-          warnings = { "underline" },
-          information = { "underline" },
-          ok = { "underline" },
-        },
-        inlay_hints = {
-          background = true,
+  name = "catppuccin",
+  config = function()
+    require("catppuccin").setup({
+      flavour = "mocha",
+      transparent_background = true,
+      term_colors = true,
+      compile_path = vim.fn.stdpath("cache") .. "/catppuccin",
+      styles = {
+        comments = { "italic" },
+        conditionals = { "italic" },
+        keywords = { "italic" },
+        types = { "italic" },
+      },
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        treesitter = true,
+        which_key = true,
+        telescope = { enabled = true },
+        illuminate = { enabled = true, lsp = false },
+        native_lsp = {
+          enabled = true,
+          virtual_text = {
+            errors = { "italic" },
+            hints = { "italic" },
+            warnings = { "italic" },
+            information = { "italic" },
+            ok = { "italic" },
+          },
+          underlines = {
+            errors = { "underline" },
+            hints = { "underline" },
+            warnings = { "underline" },
+            information = { "underline" },
+            ok = { "underline" },
+          },
+          inlay_hints = {
+            background = true,
+          },
         },
       },
-    },
-  },
-  config = function()
-    vim.cmd.colorscheme "catppuccin-mocha"
+    })
+    vim.cmd.colorscheme("catppuccin")
   end,
 }
