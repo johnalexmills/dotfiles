2183b7807c35aebecc72b846f340c0cd4f96c45b
johnalexmills <johnalexmills@icloud.com>
2025-09-01 10:11:18 -0400

    adjusting fidget to show messages longer

--- nvim/.config/nvim/lua/plugins/fidget.lua
+++ nvim/.config/nvim/lua/plugins/fidget.lua
@@ -4,9 +4,9 @@ return {
   opts = {
     progress = {
       display = {
-        done_ttl = 3,
+        done_ttl = 5,
         done_icon = "✔",
-        progress_ttl = 5,
+        progress_ttl = 8,
         progress_icon = { pattern = "dots", period = 1 },
       },
     },
@@ -18,6 +18,22 @@ return {
         relative = "editor",
         x_padding = 1,
         y_padding = 1,
+      },
+      view = {
+        stack_upwards = true,
+        icon_separator = " ",
+        group_separator = "---",
+        group_separator_hl = "Comment",
+      },
+      configs = {
+        default = vim.tbl_extend("force", require("fidget.notification").default_config, {
+          name = "Fidget",
+          icon = "🔔",
+          ttl = 10, -- Keep notifications longer
+          render = function(notif)
+            return string.format("[%s] %s", notif.name or "LSP", notif.message)
+          end,
+        }),
       },
     },
   },
