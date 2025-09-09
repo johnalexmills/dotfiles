183de63c03985543ba283b719617c205a24ffce5
johnalexmills <johnalexmills@icloud.com>
2025-09-09 16:11:18 -0400

    increasing width of vertical terminal

--- nvim/.config/nvim/lua/plugins/toggleterm.lua
+++ nvim/.config/nvim/lua/plugins/toggleterm.lua
@@ -13,7 +13,7 @@ return {
       if term.direction == "horizontal" then
         return 15
       elseif term.direction == "vertical" then
-        return vim.o.columns * 0.4
+        return vim.o.columns * 0.45
       end
       return 20
     end,
