56a4c4d0b72dc98061da47a581aa6c2fe6e72811
johnalexmills <johnalexmills@icloud.com>
2025-09-12 07:26:10 -0400

    removing buttons from alpha

--- nvim/.config/nvim/lua/plugins/alpha.lua
+++ nvim/.config/nvim/lua/plugins/alpha.lua
@@ -22,17 +22,15 @@ return {
       [[                                                                       ]],
       [[                                                                       ]],
     }
-    dashboard.section.buttons.val = {
-      dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
-      dashboard.button("e", " " .. " New file", ":ene <BAR> startinsert <CR>"),
-      dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
-      dashboard.button("t", " " .. " Find text", ":Telescope live_grep <CR>"),
-      dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
-      dashboard.button("q", " " .. " Quit", ":qa<CR>"),
-    }
+    dashboard.section.buttons.val = {}
 
     dashboard.section.header.opts.hl = "Include"
-    dashboard.section.buttons.opts.hl = "Keyword"
+
+    dashboard.config.layout = {
+      { type = "padding", val = vim.fn.max { 2, vim.fn.floor(vim.fn.winheight(0) * 0.2) } },
+      dashboard.section.header,
+      { type = "padding", val = 2 },
+    }
 
     dashboard.opts.opts.noautocmd = true
     alpha.setup(dashboard.opts)
