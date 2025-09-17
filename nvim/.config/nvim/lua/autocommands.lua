fc3b56b766578590988678d82f0170d567c60205
johnalexmills <johnalexmills@icloud.com>
2025-09-17 13:48:35 -0400

    updating spellchecking

--- nvim/.config/nvim/lua/autocommands.lua
+++ nvim/.config/nvim/lua/autocommands.lua
@@ -10,7 +10,7 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 })
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "gitcommit", "markdown" },
+  pattern = { "gitcommit", "markdown", "text", "tex", "mail", "rst", "asciidoc", "org" },
   callback = function()
     vim.opt_local.wrap = true
     vim.opt_local.spell = true
