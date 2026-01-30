d8e2162d1b415d737c305b8ffe2f2c3dc9ce059d
Alex Mills <alexmills@Alexs-MacBook-Air.local>
2026-01-30 16:32:34 -0500

    adding floating terminal option through snacks

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -3,10 +3,8 @@ return {
   priority = 1000,
   event = "VeryLazy",
   keys = {
-    { "<leader>T", group = "Terminal" },
-    { "<leader>Tf", function() Snacks.terminal() end, desc = "Float Terminal" },
+    { "<leader>T", function() Snacks.terminal(nil, { win = { position = "float" } }) end, desc = "Float Terminal" },
     { "<C-\\>", function() Snacks.terminal() end, desc = "Toggle Terminal", mode = { "n", "t" } },
-    { "<leader>g", group = "Git" },
     { "<leader>gg", function() Snacks.terminal("lazygit", { cwd = Snacks.git.get_root() }) end, desc = "LazyGit" },
   },
   opts = {
